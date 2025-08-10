4e11582e976440c87a434b5faeb23bdadd0ba12b
MarceloSavian <marcelo.savian@gmail.com>
2025-08-10 22:38:02 +0200

    fix: remove unused code

--- functions/infra/services/EmailService.ts
+++ functions/infra/services/EmailService.ts
@@ -1,17 +1,9 @@
 import { Resource } from 'sst';
 import { IEmailService } from '../../data/domain/EmailService';
-import { CreateEmailIdentityCommand, SESv2Client, SendEmailCommand } from '@aws-sdk/client-sesv2';
+import { SESv2Client, SendEmailCommand } from '@aws-sdk/client-sesv2';
 
 export class EmailService implements IEmailService {
   constructor(private readonly sesv2Client: SESv2Client) {}
-
-  async verify(email: string): Promise<undefined> {
-    await this.sesv2Client.send(
-      new CreateEmailIdentityCommand({
-        EmailIdentity: email,
-      }),
-    );
-  }
 
   async sendSimple(subject: string, text: string, email: string): Promise<undefined> {
     await this.sesv2Client.send(
